10f0635dac067cffabf9b7a0a03eff4bb6b984c8
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-08-03 21:54:40 +0000

    Add pagination to recipes

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+import math
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'myCookeys'
@@ -8,15 +9,54 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI',
                                     "mongodb+srv://admin:1studentDeveloper@firstcluster.b5ihz.mongodb.net/myCookeys?retryWrites=true&w=majority")
 
 mongo = PyMongo(app)
+recipesPerPage = 9
+recipe_count = mongo.db.recipes.count()
+
+
+def getNumberOfPages(count):
+    return math.ceil(count/recipesPerPage)
 
 
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
     """ render all recipes """
+    currentpage = 1
+    pagelessone = int(currentpage)-1
     return render_template('recipes.html',
-                           recipes=mongo.db.recipes.find(),
+                           recipesPerPage=recipesPerPage,
+                           currentpage=1,
+                           recipes=mongo.db.recipes.find().skip(recipesPerPage*(pagelessone)),
+                           numberOfPages=getNumberOfPages(recipe_count),
                            title='Recipes')
+
+
+@app.route('/get_recipes/<currentpage>')
+def get_recipes_set(currentpage):
+    """ render specific set of recipes """
+    pagelessone = int(currentpage)-1
+    return render_template('recipes.html',
+                           recipesPerPage=recipesPerPage,
+                           currentpage=currentpage,
+                           recipes=mongo.db.recipes.find().skip(recipesPerPage*(pagelessone)),
+                           numberOfPages=getNumberOfPages(recipe_count),
+                           title='Recipes')
+
+
+@app.route('/get_recipes/<currentpage>')
+def get_next_page(currentpage):
+    newcurrentpage = int(currentpage) - 1
+    return redirect(url_for('get_recipes_set',
+                            currentpage=newcurrentpage),
+                    recipesPerPage=recipesPerPage)
+
+
+@app.route('/get_recipes/<currentpage>')
+def get_prev_page(currentpage):
+    newcurrentpage = int(currentpage) + 1
+    return redirect(url_for('get_recipes_set',
+                            currentpage=newcurrentpage),
+                    recipesPerPage=recipesPerPage)
 
 
 if __name__ == '__main__':
--- unititest/testrecipes.py
+++ unititest/testrecipes.py
@@ -2,6 +2,7 @@ import unittest
 import os
 from flask import Flask, request
 from flask_pymongo import PyMongo
+from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv('MONGO_URI',
@@ -19,6 +20,21 @@ def test_1_get_all_recipes(self):
     r = request.get(APITest.API_URL)
     self.assertEqual(r.status_code, 200)  # ok
     self.assertEqual(len(mongo.db.recipes.find(), 2))
+
+
+# return true if prev, next and page1 are displayed for pagination
+# https://www.techbeamers.com/locate-elements-selenium-python/
+def test_2_check_firstpage_pagination(self):
+    self.assertTrue(self.is_element_present(By.ID, "prev"))
+    self.assertTrue(self.is_element_present(By.ID, "next"))
+    self.assertTrue(self.is_element_present(By.ID, "recipepage1"))
+
+
+def test_3_check_items_in_page(self):
+    numberPerPage = 9
+    self.expected = mongo.db.recipes.find().limit(numberPerPage)
+    self.result = ['baz', 'foo', 'bar']
+    self.assertCountEqual(self.result, self.expected)
 
 
 if __name__ == '__main__':
