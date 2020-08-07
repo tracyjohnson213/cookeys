813cc2fb38bd520519e14093660c1ec01bfe5132
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2020-08-06 21:46:41 -0700

    Fix pagination next and previous buttons

--- run.py
+++ run.py
@@ -2,16 +2,18 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
 import math
+from datetime import datetime
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'myCookeys'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI',
                                     "mongodb+srv://admin:1studentDeveloper@firstcluster.b5ihz.mongodb.net/myCookeys?retryWrites=true&w=majority")
-
 mongo = PyMongo(app)
+now = datetime.now()
 recipesPerPage = 9
 recipe_count = mongo.db.recipes.count_documents({})
+category_count = mongo.db.categories.count_documents({})
 
 
 def getNumberOfPages(count):
@@ -19,50 +21,37 @@ def getNumberOfPages(count):
     return math.ceil(count/recipesPerPage)
 
 
+def getCountOfDbItems(database,databaseItem):
+    dbase = mongo.db.database.find({databaseItem: databaseItem})
+    return dbase.count_documents({})
+
+
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
     """ render all recipes """
     currentpage = 1
-    pagelessone = int(currentpage)-1
+    numberOfPages = getNumberOfPages(recipe_count)
     return render_template('recipes.html',
                            recipesPerPage=recipesPerPage,
                            currentpage=1,
                            recipes=mongo.db.recipes.find()
-                           .skip(recipesPerPage*(pagelessone)),
-                           numberOfPages=getNumberOfPages(recipe_count),
+                           .skip(recipesPerPage*(int(currentpage)-1)),
+                           numberOfPages=numberOfPages,
                            title='Recipes')
 
 
 @app.route('/get_recipes/<currentpage>')
 def get_recipes_set(currentpage):
     """ render specific set of recipes """
-    pagelessone = int(currentpage)-1
+    numberOfPages = getNumberOfPages(recipe_count)
     return render_template('recipes.html',
                            recipesPerPage=recipesPerPage,
                            currentpage=currentpage,
                            recipes=mongo.db.recipes.find()
-                           .skip(recipesPerPage*(pagelessone)),
-                           numberOfPages=getNumberOfPages(recipe_count),
+                           .skip(recipesPerPage*(int(currentpage)-1)),
+                           numberOfPages=numberOfPages,
                            title='Recipes')
-
-
-@app.route('/get_recipes/<currentpage>')
-def get_next_page(currentpage):
-    """ calculate for next page button """
-    newcurrentpage = int(currentpage) - 1
-    return redirect(url_for('get_recipes_set',
-                            currentpage=newcurrentpage),
-                    recipesPerPage=recipesPerPage)
-
-
-@app.route('/get_recipes/<currentpage>')
-def get_prev_page(currentpage):
-    """ calculate for previous page button """
-    newcurrentpage = int(currentpage) + 1
-    return redirect(url_for('get_recipes_set',
-                            currentpage=newcurrentpage),
-                    recipesPerPage=recipesPerPage)
 
 
 @app.route('/get_cookie/<cookie_name>')
