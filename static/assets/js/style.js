89d393ef0c60be68388f84ee2d30a5db9c40b785
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2020-08-08 22:04:05 -0700

    Update jquery script to only run if add or edit recipe forms are present

--- static/assets/js/style.js
+++ static/assets/js/style.js
@@ -1,6 +1,6 @@
 // Adjust height of textareas in edit recipe form
 // https://stackoverflow.com/questions/24994395/change-the-height-and-width-of-text-area
-$(document).ready(function () {
+if ($('#addrecipe-form').length || $('#editrecipe-form').length)(function () {
   textAreaAdjust(document.getElementById("ingredients"));
   textAreaAdjust(document.getElementById("summary"));
   textAreaAdjust(document.getElementById("steps"));
